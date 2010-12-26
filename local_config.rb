4f64109da05909bb092f4049429cd0b85cbee654
unknown <Dean@.(none)>
2010-12-26 17:36:41 -0500

    Renaming some files

--- local_config.rb
+++ local_config.rb
@@ -1,10 +1,10 @@
 class LocalConfig
     def self.user_repositories
-        []
+        ["c:/book"]
     end
     
     def self.backup_repositories
-        []
+        ["c:/backup"]
     end
 
     def self.file_map_name
