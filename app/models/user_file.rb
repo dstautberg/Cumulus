66b029fee5adfb0ee790c06e30370a766d8fb5fd
Dean Stautberg <dean@stautberg.net>
2011-02-21 20:37:00 -0500

    Annotated models.

--- app/models/user_file.rb
+++ app/models/user_file.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+# Schema version: 20101227021239
+#
+# Table name: user_files
+#
+#  id         :integer         not null, primary key
+#  directory  :string(255)
+#  filename   :string(255)
+#  mtime      :datetime
+#  size       :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class UserFile < ActiveRecord::Base
 end
