cfd6e8bcdbaeed2e2245834b9063445ed6896da4
Dean Stautberg <dean@stautberg.net>
2011-08-11 19:35:39 -0400

    Put some initial stubbed-out code and comments for the user file monitor to initiate a file send to whichever node will be storing the backup.

--- lib/em_user_file_monitor.rb
+++ lib/em_user_file_monitor.rb
@@ -7,73 +7,78 @@ java_import com.hazelcast.core.EntryListener
 class EmUserFileMonitor
 
   def initialize
-    puts "UserFileMonitor: Starting"
+    Rails.logger.debug "UserFileMonitor: Starting"
     @my_node_name = Socket.gethostname.downcase
-    @q = EM::Queue.new
-    push_all_user_repositories(@q)
-    puts "UserFileMonitor.initialize complete"
+    @paths_to_process = EM::Queue.new
+    @files_needing_backup = EM::Queue.new
+    push_all_user_repositories(@paths_to_process)
+    Rails.logger.debug "UserFileMonitor.initialize complete"
   end
 
   def push_all_user_repositories(q)
     Cumulus::Application.config.user_repositories.each { |dir|
-      puts "* pushing #{dir}"
+      Rails.logger.debug "Queueing path for processing: #{dir}"
       q.push(dir)
     }
   end
 
   def tick
-    @q.pop do |path|
+    # Process one entry in the queue
+    @paths_to_process.pop do |path|
       if File.ftype(path) == "file"
         process_file(path)
       else
-        Dir.foreach(path) do |filename|
-          if not [".",".."].include?(filename)
-            fullpath = File.join(path, filename)
-            puts "* pushing #{fullpath}"
-            @q.push(fullpath)
-          end
-        end
+        process_dir(path)
       end
     end
+
+    # Send a file if we have one queued up
+    @files_needing_backup.pop do |path|
+      # Figure out what node to send the file to.
+      # Pick a random one for starters, but make sure it has enough free space to store this file.
+      # I need a model for keeping track of other nodes.
+      
+      # Initiate a TCP connection to the node's IP and port.
+      # EventMachine::connect "www.bayshorenetworks.com", 80, DumbHttpClient
+      # Send a JSON string containing the message type "file-incoming", and the file path.
+      # Receive response as a JSON string that has a status ("OK"), and a port number to send to.
+      # Initiate another connection to the new port and send the file data.
+      # Note: the initial connection will be triggered here, and we will give it a FileTransferHandler
+      # object that is initialized with the file path to send.  The response will be handled by that instance,
+      # and it will pass itself to the followup connect that sends the file data.
+    end
+
     # If we're done, then start over
-    push_all_user_repositories(@q) if @q.empty?
+    push_all_user_repositories(@paths_to_process) if @paths_to_process.empty?
   end
 
-  def process_file(full_path)
-    mtime = File.mtime(full_path)
-    size = File.size(full_path)
-    user_file = UserFile.find_by_full_path(full_path)
+  def process_file(path)
+    mtime = File.mtime(path)
+    size = File.size(path)
+    user_file = UserFile.find_by_full_path(path)
     if user_file
       updated = (mtime > user_file.mtime + 2) # use a small buffer to avoid issues with fractional seconds
-      @files_to_backup.push(full_path)
     else
       updated = true
     end
     if updated
-      puts "[#{Time.now}] Updating entry for: #{full_path}"
-      @file_map.put(key.to_yaml, entry.to_yaml)
+      Rails.logger.debug "File needs backed up: #{path}"
+      @files_needing_backup.push(path)      
     end
 
     # Add file info to the local database too
     UserFile.create!(:filename => filename, :directory => Dir.getwd, :mtime => mtime, :size => size)
   end
 
-  def process_dir(dir)
-    #puts "UserFileMonitor: Checking dir #{dir}"
-    Dir.chdir(dir) do
-      Dir.foreach(".") do |entry|
-        if entry != "." and entry != ".."
-          if File.directory?(entry)
-            process_dir(entry)
-          else
-            process_file(entry)
-          end
-        end
-        # Check all entries in the distributed map for this directory to see if anything was deleted
-        # If so, flag it in the distributed map as deleted.
-        # If it has already been flagged as deleted, and has no registered backups, then we should be clear to remove it from the map.
+  def process_dir(path)
+    Dir.foreach(path) do |filename|
+      if not [".",".."].include?(filename)
+        fullpath = File.join(path, filename)
+        Rails.logger.debug "Queueing path for processing: #{fullpath}"
+        @paths_to_process.push(fullpath)
       end
     end
+    # Check all UserFile's in this directory to see if anything was deleted.
+    # If so, notify backuppers that it was deleted.
   end
-
 end
