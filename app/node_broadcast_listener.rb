88efd24e19c31b3f3bbeae646d9f84204f31e6c6
Dean Stautberg <dean@stautberg.net>
2012-07-21 17:45:04 -0400

    Added commented-out code for handle the multicast messages

--- app/node_broadcast_listener.rb
+++ app/node_broadcast_listener.rb
@@ -1,3 +1,6 @@
+require 'socket'
+require 'ipaddr'
+
 class NodeBroadcastListener
   def initialize
     AppLogger.debug "#{self.class.to_s}: initialize complete"
@@ -5,17 +8,22 @@ class NodeBroadcastListener
 
   def start
     AppLogger.debug "#{self.class.to_s}: starting"
+
+    #MULTICAST_ADDR = "225.4.5.6" 
+    #PORT = 5000
+    #ip =  IPAddr.new(MULTICAST_ADDR).hton + IPAddr.new("0.0.0.0").hton
+    #sock = UDPSocket.new
+    #sock.setsockopt(Socket::IPPROTO_IP, Socket::IP_ADD_MEMBERSHIP, ip)
+    #sock.bind(Socket::INADDR_ANY, PORT)
+    #loop do
+    #  msg, info = sock.recvfrom(1024)
+    #  puts "MSG: #{msg} from #{info[2]} (#{info[3]})/#{info[1]} len #{msg.size}" 
+    #end
+    
+    # Check code here too: http://www.ruby-forum.com/topic/200353
   end
 
   def stop
-
-  end
-
-  def post_init
-    puts "NodeBroadcastListener: Received a new connection"
-  end
-  
-  def receive_data(data)
 
   end
 end
--- app/node_broadcaster.rb
+++ app/node_broadcaster.rb
@@ -1,3 +1,5 @@
+require 'socket'
+
 class NodeBroadcaster
   def initialize
     AppLogger.debug "#{self.class.to_s}: initialize complete"
@@ -21,5 +23,15 @@ class NodeBroadcaster
   def tick
     # Send out a broadcast packet with information about my node
     # JSON(Node.local)
+	
+    #MULTICAST_ADDR = "225.4.5.6" 
+    #PORT= 5000
+    #begin
+    #  socket = UDPSocket.open
+    #  socket.setsockopt(Socket::IPPROTO_IP, Socket::IP_TTL, [1].pack('i'))
+    #  socket.send(ARGV.join(' '), 0, MULTICAST_ADDR, PORT)
+    #ensure
+    #  socket.close 
+    #end
   end
 end
