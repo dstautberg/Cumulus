9de946603f5530ddc9398f36506efd003ceb2e7a
Dean Stautberg <dstautberg@2co.com>
2012-11-26 14:51:59 -0500

    Added script that saves file metadata to a sqlite database.

---
+++ script/file_duplicate_detector.rb
@@ -0,0 +1,51 @@
+require "find"
+require "sequel"
+require "digest"
+
+db = Sequel.sqlite("dup.db")
+db.drop_table :files
+db.create_table(:files) do
+  String :name
+  String :directory
+  Integer :size
+  DateTime :last_modified
+  String :hash
+end
+
+MAX_READ_RATE = 10000000 # bytes per second
+READ_SLEEP_TIME = 0.1
+MAX_READ_PER_LOOP = MAX_READ_RATE * READ_SLEEP_TIME
+
+def file_hash(file)
+  digest = Digest::SHA2.new
+  File.open(file, 'rb') do |f|
+    while buffer = f.read(MAX_READ_PER_LOOP)
+      digest.update(buffer)
+      sleep READ_SLEEP_TIME
+    end
+  end
+  digest.hexdigest
+end
+
+count, start = 0, Time.now
+last_output = start
+
+puts start
+Find.find("/").each do |f|
+  if File.file?(f)
+    data = {}
+    data[:directory], data[:name] = File.split(f)
+    data[:size] = File.size(f)
+    data[:last_modified] = File.mtime(f)
+    data[:hash] = file_hash(f)
+    db[:files].insert(data)
+    count += 1
+    if Time.now - last_output > 1
+      last_output = Time.now
+      files_per_second = count.to_f / (Time.now - start).to_f
+      print "Files: %d, DB Size: %0.1f KB, Files per sec: %0.1f\r" % [count, File.size("dup.db")/1024.0, files_per_second]
+    end
+  end
+end
+puts
+puts Time.now
