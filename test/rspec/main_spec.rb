8e7dbaf81ad79ec80c672b0bd69b953c229f0afb
Dean Stautberg <dean@stautberg.net>
2013-04-07 19:02:30 -0400

    Added a timeout to the main spec to keep it from hanging.

--- test/rspec/main_spec.rb
+++ test/rspec/main_spec.rb
@@ -2,12 +2,12 @@ require_relative "../test_helper"
 
 describe Main do
   it "starts up and shuts down without errors" do
-	AppConfig.user_repositories = ["tmp"]
+    AppConfig.user_repositories = ["tmp"]
     AppLogger.debug "Test calling Main.start"
-	Main.start
+    Main.start
     AppLogger.debug "Test done calling Main.start"
     AppLogger.debug "Test calling Main.stop"
-    Main.stop
+    Timeout::timeout(30) { Main.stop }
     AppLogger.debug "Test done calling Main.stop"
   end
 end
